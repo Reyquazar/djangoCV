b9658c3e33ba7555f4721766d870013320a55305
Elshan Gurbanov <elshan.elshan.82@gmail.com>
2023-05-04 16:30:33 +0300

    опять разбираюсь

--- MyCV/MyCV/settings.py
+++ MyCV/MyCV/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in
+# production secret!
 SECRET_KEY = 'django-insecure-o=5%bbvcbvdgbfcfeert$@d#fch#hq^p+6k&(iatd4zjihu*8n8j7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
